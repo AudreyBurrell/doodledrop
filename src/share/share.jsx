11c9969a65d92e360a7279b6af642c3e78131014
AudreyBurrell <audreyb2165@gmail.com>
2025-02-06 20:18:29 -0700

    Making the share.jsx working

--- src/share/share.jsx
+++ src/share/share.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './share.css';
 
 export function Share() {
+  const navigate = useNavigate();
+
   return (
     <main>
-      <div>share displayed here</div>
+      <div>
+        <form id="sendForm">
+          <label htmlFor="recipientEmail">Enter recipient's email: </label>
+          <input type="email" id="recipientEmail" name="recipientEmail" placeholder="Enter email" required />
+          
+          <div id="imageToSend">
+            <p>Image to send...</p>
+            <img src="placeholder1.png" alt="placeholder" width="100" />
+          </div>
+          
+          <button type="submit" className="btn btn-outline-success">
+            <b>Send</b>
+          </button>
+          
+          <br /><br />
+          
+          <button 
+            type="button" 
+            className="btn btn-outline-primary" 
+            id="backButton" 
+            onClick={() => navigate('/draw')}
+          >
+            <b>&larr;</b>
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
