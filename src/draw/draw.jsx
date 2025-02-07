1d47eb2a3062547b5dcd9cf8f8fb02ff0929e54f
AudreyBurrell <audreyb2165@gmail.com>
2025-02-06 20:12:17 -0700

    Getting the stuff for draw.jsx to actually show up

--- src/draw/draw.jsx
+++ src/draw/draw.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
-import './draw.css'
+import { useNavigate } from 'react-router-dom';
+import './draw.css';
 
 export function Draw() {
+  const navigate = useNavigate();
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
+    <main>
       <div>
-        <canvas id="drawCanvas" width = "400" height ="400" class="container-fluid" style="border:1px solid black;">
-            </canvas>
-            <br />
-            <div id="controls">
-                <label for="colors">Color:</label>
-                <select name="colors" id="colors">
-                    <option value="red">Red</option>
-                    <option value="yellow">Yellow</option>
-                    <option value="blue">Blue</option>
-                    <option value="black">Black</option>
-                </select>
-                <label for="size">Size:</label>
-                <select name="size" id="size">
-                    <option value="small">Small</option>
-                    <option value="medium">Medium</option>
-                    <option value="large">Large</option>
-                </select>
-            </div>
-            <div id="buttons">
-                <button type="button" class="btn btn-outline-danger" id="clearButton"><b>clear</b></button>
-                <button type="button" class= "btn btn-outline-primary"onclick="location.href='gallery.html';">gallery</button>
-                <button type="button" class="btn btn-outline-success"onclick="location.href='share.html';">share</button>
-            </div>
+        <canvas
+          id="drawCanvas"
+          width="400"
+          height="400"
+          className="container-fluid"
+          style={{ border: '1px solid black' }}
+        ></canvas>
+        <br />
+        <div id="controls">
+          <label htmlFor="colors">Color:</label>
+          <select id="colors">
+            <option value="red">Red</option>
+            <option value="yellow">Yellow</option>
+            <option value="blue">Blue</option>
+            <option value="black">Black</option>
+          </select>
+          <label htmlFor="size">Size:</label>
+          <select id="size">
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
         </div>
+        <div id="buttons">
+          <button type="button" className="btn btn-outline-danger">
+            <b>Clear</b>
+          </button>
+          <button type="button" className="btn btn-outline-primary" onClick={() => navigate('/gallery')}>
+            Gallery
+          </button>
+          <button type="button" className="btn btn-outline-success" onClick={() => navigate('/share')}>
+            Share
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
