537f38bc817c5d216fa972ab5f0718cb0c394e98
AudreyBurrell <audreyb2165@gmail.com>
2025-02-06 20:16:20 -0700

    Getting the gallery.jsx to work (adding the html stuff into it)

--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './gallery.css';
 
 export function Gallery() {
+  const navigate = useNavigate();
+
   return (
     <main>
-      <div>gallery displayed here</div>
+      <div className="gallery-grid">
+        <div className="item">
+          <img src="placeholder1.png" alt="placeholder" width="100" />
+        </div>
+        <div className="item">
+          <img src="placeholder2.png" alt="placeholder" width="100" />
+        </div>
+      </div>
+      <button 
+        type="button" 
+        id="backButton" 
+        className="btn btn-outline-primary" 
+        onClick={() => navigate('/draw')}>
+        &larr;
+      </button>
     </main>
   );
 }
