9d45efb6ea0b632b8b0823962cb8f544232c38a2
AudreyBurrell <audreyb2165@gmail.com>
2025-02-06 19:47:37 -0700

    Realizing that username should be required and also moving the html from login.html to login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+      <div>
+        <form method="get" action="draw.html">
+                <div>
+                    <span>Username: </span>
+                    <input type="text" placeholder="username here" required />
+                </div>
+                <br />
+                <button class="btn btn-outline-primary">Login &rarr;</button>
+                <button class="btn btn-outline-success">Register &rarr;</button>
+            </form>
+            <br />
+      </div>
     </main>
   );
 }
