d048d1daa881297cd8e286ad8e04629d1393e7da
AudreyBurrell <audreyb2165@gmail.com>
2025-02-06 20:41:20 -0700

    Making the buttons on login actually work (finally!)

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import './login.css'
-
+import { useNavigate } from 'react-router-dom';
+import './login.css';
 
 export function Login() {
+  const navigate = useNavigate();
+
   return (
     <main>
       <div>
-        <form method="get" action="draw.html">
-                <div>
-                    <span>Username: </span>
-                    <input type="text" placeholder="username here" required />
-                </div>
-                <br />
-                <button className="btn btn-outline-primary">Login &rarr;</button>
-                <button className="btn btn-outline-success">Register &rarr;</button>
-            </form>
-            <br />
+        <form>
+          <div>
+            <span>Username: </span>
+            <input type="text" placeholder="username here" required />
+          </div>
+          <br />
+          <button type="button" className="btn btn-outline-primary" onClick={() => navigate('/draw')}>
+            Login &rarr;
+          </button>
+          <button type="button" className="btn btn-outline-success" onClick={() => navigate('/draw')}>
+            Register &rarr;
+          </button>
+        </form>
+        <br />
       </div>
     </main>
   );
 }
+
 
