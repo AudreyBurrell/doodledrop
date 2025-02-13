ab5e9370120b21fecc5e0860c181e7814d65bb78
AudreyBurrell <audreyb2165@gmail.com>
2025-02-13 09:48:34 -0700

    Doing the reactivity for login- the required thing actually works now! Also the buttons are disabled until something is typed into the username field.

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 export function Login() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (username.trim() === '') {
+      return;
+    }
+    navigate('/draw')
+  }
 
   return (
     <main>
       <div>
-        <form>
+        <form onSubmit = {(e) => e.preventDefault()}>
           <div>
             <span>Username: </span>
-            <input type="text" placeholder="username here" required />
+            <input
+              type = "text"
+              placeholder = "username here"
+              value={username}
+              onChange={handleInputChange}
+              required>
+            </input>
           </div>
           <br />
-          <button type="button" className="btn btn-outline-primary" onClick={() => navigate('/draw')}>
+          <button //login button
+            type="button"
+            className = "btn btn-outline-primary"
+            onClick={handleSubmit}
+            disabled={!username.trim()}
+          >
             Login &rarr;
           </button>
-          <button type="button" className="btn btn-outline-success" onClick={() => navigate('/draw')}>
-            Register &rarr;
+          <button //register button
+            type="button"
+            className="btn btn-outline-success"
+            onClick={handleSubmit}
+            disabled={!username.trim()}>
+              Register &rarr;
           </button>
         </form>
-        <br />
       </div>
     </main>
   );
