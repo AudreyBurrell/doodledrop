4756087c7d270f5fc2519b652d3723bf8fbc15cd
AudreyBurrell <audreyb2165@gmail.com>
2025-03-08 12:15:22 -0700

    creating a new user

--- service/index.js
+++ service/index.js
@@ -4,12 +4,28 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
+
+let users=[];
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
 let apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+//Create a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('username', req.body.username)) {
+        res.status(409).send({ msg: 'Username already taken' });
+    } else {
+        const user = await createUser(req.body.username);
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
+    }
+});
+
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
