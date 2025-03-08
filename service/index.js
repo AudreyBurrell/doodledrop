9c3d36eb5a29fd9b95675a38b8946c2086d4cc39
AudreyBurrell <audreyb2165@gmail.com>
2025-03-08 11:55:46 -0700

    adding more of the basic things to get started (the app.use and the apiRouter)

--- service/index.js
+++ service/index.js
@@ -1,11 +1,17 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'DoodleDrop' });
-});
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
