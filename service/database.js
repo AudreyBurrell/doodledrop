ce239a091e9131445811a9b0319ca1d73960e510
AudreyBurrell <audreyb2165@gmail.com>
2025-03-13 09:06:23 -0600

    reinstalling mongodb so it matches the version in simon

--- service/database.js
+++ service/database.js
@@ -5,11 +5,11 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 
 const client = new MongoClient(url);
 
-// (async function testConnection() { //this will asynchronously test the connection and test the process if it fails
-//     try {
-//       await db.command({ ping: 1 });
-//     } catch (ex) {
-//       console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-//       process.exit(1);
-//     }
-//   })();
+(async function testConnection() { //this will asynchronously test the connection and test the process if it fails
+    try {
+      await db.command({ ping: 1 });
+    } catch (ex) {
+      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+      process.exit(1);
+    }
+  })();
