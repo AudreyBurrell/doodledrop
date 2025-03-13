2a0aba9a2d1a2028e03e2de950db0530b5c4378f
AudreyBurrell <audreyb2165@gmail.com>
2025-03-13 09:27:37 -0600

    making const db

--- service/database.js
+++ service/database.js
@@ -4,12 +4,29 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
 const client = new MongoClient(url);
+const db = client.db('doodledrop');
 
-(async function testConnection() { //this will asynchronously test the connection and test the process if it fails
-    try {
-      await db.command({ ping: 1 });
-    } catch (ex) {
-      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-      process.exit(1);
-    }
-  })();
+// async function connectToDatabase() {
+//     try {
+//         await client.connect();
+//         db = client.db(config.dbName);
+//         console.log('Connected to database successfully!')
+//     } catch (ex) {
+//         console.log(`Unable to connect to datablse with ${url} because ${ex.message}`);
+//         process.exit(1)
+//     }
+// }
+// (async function testConnection() { //this will asynchronously test the connection and test the process if it fails
+//     try {
+//       await db.command({ ping: 1 });
+//     } catch (ex) {
+//       console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+//       process.exit(1);
+//     }
+//   })();
+
+//   connectToDatabase();
+
+//   module.exports = {
+//     client
+//   };
