8fd270e9c122f651aa30fcc40ddd71f53700720d
AudreyBurrell <audreyb2165@gmail.com>
2025-03-13 09:28:52 -0600

    adding the asynchronously test the connection thing

--- service/database.js
+++ service/database.js
@@ -6,27 +6,12 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('doodledrop');
 
-// async function connectToDatabase() {
-//     try {
-//         await client.connect();
-//         db = client.db(config.dbName);
-//         console.log('Connected to database successfully!')
-//     } catch (ex) {
-//         console.log(`Unable to connect to datablse with ${url} because ${ex.message}`);
-//         process.exit(1)
-//     }
-// }
-// (async function testConnection() { //this will asynchronously test the connection and test the process if it fails
-//     try {
-//       await db.command({ ping: 1 });
-//     } catch (ex) {
-//       console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-//       process.exit(1);
-//     }
-//   })();
+(async function testConnection() { //this will asynchronously test the connection and test the process if it fails
+    try {
+      await db.command({ ping: 1 });
+    } catch (ex) {
+      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+      process.exit(1);
+    }
+  })();
 
-//   connectToDatabase();
-
-//   module.exports = {
-//     client
-//   };
